ee7af135735cf62b67cb000d38b720b11f42fa98
Dmitry Fedyuk <dfediuk@gmail.com>
2018-10-18 02:07:06 +0600

    0.1.2: https://github.com/kitchencollection-com/core/issues/19

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -1,12 +1,24 @@
-// 2018-10-18
-// "An ADA issue: keyboard-only users are unable to navigate to the «SIGN-IN» dropdown button":
-// https://github.com/kitchencollection-com/core/issues/17
 require(['jquery'], function($) {$(function() {
-	var $a = $('#authorization-trigger');
-	$a.attr('tabindex', 0);
-	$a.keypress(function(e) {
-		if (13 === e.keyCode) {
-			$a.click();
-		}
-	});
+	// 2018-10-18
+	// "An ADA issue: keyboard-only users are unable to navigate to the «SIGN-IN» dropdown button":
+	// https://github.com/kitchencollection-com/core/issues/17
+	$('#authorization-trigger')
+		.attr('tabindex', 0)
+		.keypress(function(e) {
+			if (13 === e.keyCode) {
+				$(this).click();
+			}
+		})
+	;
+	// 2018-10-18
+	// "An ADA issue: keyboard-only users are unable to navigate to the hamburger menu button":
+	// https://github.com/kitchencollection-com/core/issues/19
+	$('.cdz-main-menu')
+		.attr('tabindex', 0)
+		.keypress(function(e) {
+			if (13 === e.keyCode) {
+				$('.cdz-menu', this).toggle();
+			}
+		})
+	;
 });});
